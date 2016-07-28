070ba5209720401ab78104c63249c75fee43a094
canlasm <michael.canlas@foxnews.com>
2016-07-28 15:44:28 -0400

    update codes

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -4,9 +4,9 @@
 
 import time
 import psycopg2
+import bleach
 
 ## Database connection
-DB = []
 
 ## Get posts from database.
 def GetAllPosts():
@@ -20,7 +20,7 @@ def GetAllPosts():
     DB = psycopg2.connect("dbname=forum")
     c = DB.cursor()
     c.execute("SELECT time, content FROM posts ORDER BY time DESC")
-    posts = ({'content': str(row[1]), 'time': str(row[0])} for row in c.fetchall())
+    posts = ({'content': str(bleach.clean(row[1])), 'time': str(row[0])} for row in c.fetchall())
     DB.close()
     return posts
 
@@ -33,6 +33,6 @@ def AddPost(content):
     '''
     DB = psycopg2.connect("dbname=forum")
     c = DB.cursor()
-    c.execute("INSERT INTO posts (content) VALUES ('%s')" % content) 
+    c.execute("INSERT INTO posts (content) VALUES (%s)",  (bleach.clean(content),))
     DB.commit()
     DB.close()
